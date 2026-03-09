b1430b31ef911cfee5d642c7b77e99db4a2ff522
usman1170 <usman.zrr@gmail.com>
2026-03-09 15:32:11 +0500

    init banking app ui

---
+++ banking_app_ui/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
